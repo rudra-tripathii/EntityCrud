8d29e59e6c9bc9a97fcd828effa2176061f432f2
rudra-tripathii <rudra.tripathi2004@gmail.com>
2025-05-16 13:14:51 +0530

    Add project files.

---
+++ EntityCrud/Migrations/202505151748050_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿namespace EntityCrud.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Author = c.String(),
+                        ISBN = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Books");
+        }
+    }
+}
---
+++ EntityCrud/Models/LibraryDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EntityCrud.Models
+{
+    public class LibraryDBContext:DbContext
+    {
+        public LibraryDBContext():base("LibraryDBContext")
+            {
+            }
+        public DbSet<Book> Books { get; set; }
+    }
+}
